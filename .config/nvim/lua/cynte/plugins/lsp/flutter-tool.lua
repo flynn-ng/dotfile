df251ac07a146a15e93364d131c46b6c0070f843
Cynte <ng.flynn.phong@gmail.com>
2026-05-12 10:48:55 +0700

    clear: unuse plugin

--- .config/nvim/lua/cynte/plugins/lsp/flutter-tool.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"nvim-flutter/flutter-tools.nvim",
-	lazy = false,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		-- dressing.nvim removed - using snacks.input instead
-	},
-	config = true,
-}
